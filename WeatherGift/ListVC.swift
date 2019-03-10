09c196559e5b1ad1121633f233adc65c800e918a
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-03-10 18:11:31 -0400

    7.2 Complete

---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Blake Mazzaferro on 3/10/19.
+//  Copyright © 2019 Blake Mazzaferro. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
