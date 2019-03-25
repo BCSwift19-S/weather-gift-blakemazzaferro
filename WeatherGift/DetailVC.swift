ac0e1a2bbc7a2f1cfdf33f51fc338d55aaf9fc70
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-03-24 20:02:15 -0400

    7.22 Complete

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -9,6 +9,12 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE, MMM dd, y"
+    return dateFormatter
+}()
+
 class DetailVC: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
@@ -44,7 +50,8 @@ class DetailVC: UIViewController {
     func updateUserInterface() {
         let location = locationsArray[currentPage]
         locationLabel.text = location.name
-        let dateString = formatForTimeZone(unixDate: location.currentTime, timeZone: location.timeZone)
+        //let dateString = formatForTimeZone(unixDate: location.currentTime, timeZone: location.timeZone)
+        let dateString = location.currentTime.format(timeZone: location.timeZone, dateFormatter: dateFormatter)
         dateLabel.text = dateString
         temperatureLabel.text = location.currentTemp
         summaryLabel.text = location.currentSummary
@@ -52,15 +59,13 @@ class DetailVC: UIViewController {
         tableView.reloadData()
     }
     
-    func formatForTimeZone(unixDate: TimeInterval, timeZone: String) -> String {
-        let usableDate = Date(timeIntervalSince1970: unixDate)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE, MMM dd, y"
-        dateFormatter.timeZone = TimeZone(identifier: timeZone)
-        let dateString = dateFormatter.string(from: usableDate)
-        return dateString
+//    func formatForTimeZone(unixDate: TimeInterval, timeZone: String) -> String {
+//        let usableDate = Date(timeIntervalSince1970: unixDate)
+//        dateFormatter.timeZone = TimeZone(identifier: timeZone)
+//        let dateString = dateFormatter.string(from: usableDate)
+//        return dateString
+//    }
     }
-}
 
 extension DetailVC: CLLocationManagerDelegate {
     func getLocation() {
